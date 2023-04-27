d64a40420d2958dade4f5af8d698df3b1a079c05
Antoine <antoine.macheda@ynov.com>
2023-04-27 11:21:57 +0200

    Add Watch

--- MovieAPiVueJS/src/views/SearchResults.vue
+++ MovieAPiVueJS/src/views/SearchResults.vue
@@ -17,23 +17,22 @@ import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 
-  const route = useRoute()
+const route = useRoute()
 const query = ref(route.params.query)
 let movies = ref([{ id: null, title: null, overview: null }])
 
 const apiKey = 'c800a4a3cdad2bcff8ab7652fc3fa99b';
 const apiUrl = 'https://api.themoviedb.org/3/search/movie';
-const searchMovies = (query) => fetch(`${apiUrl}?api_key=${apiKey}&query=${query}`).then(res => res.json());
+const searchMovies = (query) => fetch(`${apiUrl}?api_key=${apiKey}&query=${query}`).then(res => res.json())
 
 
-  searchMovies(query.value).then(response => {
-    // console.log(response)
-    movies = response.results
-    // console.log(movies)
-
-  }
-
-)
+watch(query, (newValue, oldValue) => {
+  searchMovies(newValue).then(response => {
+    movies.value = response.results
+  }).catch(error => {
+    console.error(error)
+  })
+})
 
 
 
